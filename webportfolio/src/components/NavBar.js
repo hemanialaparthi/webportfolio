888a296675496371f56dfba72ed51a79e1e84662
hemanialaparthi <alaparthi01@allegheny.edu>
2024-10-08 15:26:54 -0400

    edit:navbar.js

--- webportfolio/src/components/NavBar.js
+++ webportfolio/src/components/NavBar.js
@@ -62,14 +62,14 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="" />
+              <a href="https://www.linkedin.com/in/hemanialaparthi/">
+                <img src={navIcon1} alt="LinkedIn" />
               </a>
-              <a href="#">
-                <img src={navIcon2} alt="" />
+              <a href="https://github.com/hemanialaparthi">
+                <img src={navIcon2} alt="GitHub" />
               </a>
-              <a href="#">
-                <img src={navIcon3} alt="" />
+              <a href="mailto:hemanialaparthi@gmail.com">
+                <img src={navIcon3} alt="Email" />
               </a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}>
